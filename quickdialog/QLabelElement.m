0e23d64979ba252e349a9471974c1885e04ee45c
Dan VanWinkle <github@danvw.com>
2014-03-05 16:15:55 -0500

    Update QLabelElement.m
    Remove compile time if block

--- quickdialog/QLabelElement.m
+++ quickdialog/QLabelElement.m
@@ -44,11 +44,9 @@
 }
 
 -(void)setIconNamed:(NSString *)name {
-#if __IPHONE_7_0
     if ([self.image respondsToSelector:@selector(imageWithRenderingMode:)]) {
         self.image = [[UIImage imageNamed:name] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
     }
-#endif
 }
 
 
